e4cbb9b966fb0a6b599d8e8183bda6e0990d3d26
sonota88 <yosiot8753@gmail.com>
2019-04-17 06:29:13 +0900

    Add shell functions: __dir__

--- app/_up_prod.sh
+++ app/_up_prod.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+__dir__() {
+  local real_path="$(readlink --canonicalize "$0")"
+  (
+    cd "$(dirname "$real_path")"
+    pwd
+  )
+}
+
+cd "$(__dir__)"
+
 APP_ENV=production \
   ./_up.sh "$@"
--- app/_up_term.sh
+++ app/_up_term.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+__dir__() {
+  local real_path="$(readlink --canonicalize "$0")"
+  (
+    cd "$(dirname "$real_path")"
+    pwd
+  )
+}
+
+cd "$(__dir__)"
+
 gnome-terminal -e "/bin/bash -c
   \"
     ./_up.sh $1
