16532053544204c2abbdee7e79a2b7afd00c0648
sonota88 <yosiot8753@gmail.com>
2019-08-14 07:05:52 +0900

    Add common_component.js / MySelect

---
+++ app/public/js/common_component.js
@@ -0,0 +1,21 @@
+class MySelect {
+  static render(items, opts){
+    const attrs = {};
+    if ("onchange" in opts) {
+      attrs.onchange = opts.onchange;
+    }
+
+    return TreeBuilder.build(h =>
+      h("select", attrs
+      , items.map(item => {
+            const optAttrs = { value: item.value };
+            if (item.value === String(opts.selected)) {
+              optAttrs.selected = "selected";
+            }
+            return h("option", optAttrs, item.label);
+          }
+        )
+      )
+    );
+  }
+}
