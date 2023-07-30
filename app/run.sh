3c9178c3c7fac050117d86213ed9ea8cd8b14399
sonota88 <yosiot8753@gmail.com>
2023-07-30 19:01:16 +0900

    Set 'use' as default command (run.sh)

--- app/run.sh
+++ app/run.sh
@@ -37,7 +37,13 @@ cmd_down(){
   curl http://localhost:${port}/shutdown
 }
 
-cmd="$1"; shift
+cmd=
+if [ $# -ge 1 ]; then
+  cmd="$1"; shift
+else
+  cmd="up"
+fi
+
 case $cmd in
   up)
     port=$(echo_port "$1"); shift
